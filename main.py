947af7e7f6a3cff69fef5cfffed173e01bdcdca8
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-26 14:12:05 -0800

    The main.py code structure was added

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from contacts import Contacts
 from menu import MainMenu
 
-# To do...
+if __name__ == '__main__':
+    while True:
+        match(1):
+            case 1: # add contact
+                pass
+            case 2: # contact list
+                pass
+            case 3: # search contacr
+                pass
+            case 4: # update contact
+                pass
+            case 5: # close app
+                print("\nGood Bye!")
+                break
