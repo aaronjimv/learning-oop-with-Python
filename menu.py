cea3ae2932813340d65ea0657f16222ff2337893
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-25 19:34:13 -0800

    showMainMenu() was added to menu.py

--- menu.py
+++ menu.py
@@ -1,2 +1,19 @@
 class MainMenu():
-    pass
+    
+    @staticmethod
+    def showMainMenu():
+        print("-------------------")
+        print("  WORKS CONTACTS   ")
+        print("-------------------")
+        print("1. Add contact")
+        print("2. Show all contacts")
+        print("3. Search contact")
+        print("4. Edit contact")
+        print("5. Close app")
+        
+        option = int(input("Choose one option: "))
+        while option > 5 or option < 1:
+            print("Invalid option...")
+            option = int(input("Choose one option: "))
+        else:
+            return option
