a60882a3a2b65e35e2c273cbc6eccf0a1fb78adf
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-26 13:38:06 -0800

    add_contact and update_contact methos was added to BaseClass class

--- contacts.py
+++ contacts.py
@@ -1,5 +1,13 @@
 class BaseClass:
-    pass
+    contacts = [] # contacts list
+
+    # save a contact in the list
+    def add_contact(self, name, email, phone):
+        user_data = {"name": name, "email": email, "phone": phone}
+        self.contacts.append(user_data)
+
+    def update_contact(self):
+        pass
 
 class Contacts(BaseClass):
     pass
