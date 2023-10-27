75c384b9ffaa27727b2197c6167341f8849ede17
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-27 12:13:32 -0800

    comments with concepts was added to 06-setter-getter.py

--- basic_concepts/06-setter-getter.py
+++ basic_concepts/06-setter-getter.py
@@ -1,9 +1,38 @@
+"""
+In object-oriented programming (OOP), getters and setters 
+are methods that allow you to access and modify the values 
+of private attributes in a class. Private attributes are those 
+that are not meant to be accessed or modified directly from outside the class.
+"""
+
+"""
+A getter is a method that returns the value of a private attribute. 
+It allows you to access the value of the attribute without exposing it directly. 
+
+A setter is a method that sets the value of a private attribute. 
+It allows you to modify the value of the attribute without exposing it directly.
+"""
+
+"""
+@property is a decorator, indicating that the next method is a 
+property of the class. A property is a method that can be accessed 
+as if it were an attribute, that is, without using parentheses. 
+Properties serve to control access to the private attributes of 
+the class, allowing them to be read or modified as desired.
+"""
+
+"""
+Another decorator used is @name.setter and @lastname.setter, which 
+indicates that the next method is a setter of the name and lastname property. 
+"""
+
 class User:
     def __init__(self, name, lastname, password):
-        # atributos publicos
-        self._name = name
-        self._lastname = lastname
-        self._password = self.encryptPassword(password)
+        # Private attributes
+        self.__name = name
+        self.__lastname = lastname
+        # Public attributes
+        self.password = self.encryptPassword(password)
 
     def encryptPassword(self, password):
         pass
@@ -14,28 +43,27 @@ class User:
     # getter
     @property
     def name(self):
-        return self._name
+        return self.__name
     
     # setter
     @name.setter
     def name(self, name):
-        self._name = name
+        self.__name = name
 
     # getter
     @property
     def lastname(self):
-        return self._lastname
+        return self.__lastname
     
     # setter
     @lastname.setter
     def lastname(self, lastname):
-        self._lastname = lastname
+        self.__lastname = lastname
 
 
-# instancia
 user1 = User("Echiro", "Oda", "test")
 print(user1.name)
-print(user1.name)
+print(user1.lastname)
 
 print("---")
 
