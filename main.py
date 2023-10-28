23f63f3e482f079363200c11ea4a0512b2655b3f
Aaron Jimenez <aaronjimv@gmail.com>
2023-10-28 12:34:31 -0800

    snake case was applied to methods in main.py

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from menu import MainMenu
 
 if __name__ == '__main__':
     while True:
-        option = MainMenu.showMainMenu()
+        option = MainMenu.show_main_menu()
         contacts = Contacts()
 
         match(option):
             case 1:  # add contact
-                MainMenu.showMenuAddContact()
-                name, email, phone = MainMenu.addConact()
+                MainMenu.show_menu_add_contact()
+                name, email, phone = MainMenu.add_conact()
                 contacts.add(name, email, phone)
             case 2:  # contact list
-                MainMenu.showMenuAllContact()
+                MainMenu.show_menu_all_contact()
                 contacts.show_all_contacts()
             case 3:  # search contacr
-                MainMenu.showMenuSearchContact()
-                email = MainMenu.searchContact()
+                MainMenu.show_menu_search_contact()
+                email = MainMenu.search_contact()
                 contact = contacts.search(email)
                 if contact:
                     print(contact)
                 else:
                     print("This contact does not exits.")
             case 4:  # update contact
-                MainMenu.showMenuUpdate()
-                MainMenu.showMenuAllContact()
+                MainMenu.show_menu_update()
+                MainMenu.show_menu_all_contact()
                 contacts.show_all_contacts()
-                email = MainMenu.searchContact()
+                email = MainMenu.search_contact()
                 contact = contacts.search(email)
                 if contact:
-                    name, phone = MainMenu.getConatactData()
+                    name, phone = MainMenu.get_conatact_data()
                     response = contacts.update(contact["email"], name, phone)
                     print("This contact was updated succefully!")
             case 5:  # close app
