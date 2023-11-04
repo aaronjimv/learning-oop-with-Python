9f354cffaa2d36fe8d0d3b366eb5950b2aa1c5b3
Aaron Jimenez <aaronjimv@gmail.com>
2023-11-03 20:10:49 -0800

    comment added to file 10

--- basic_concepts/10-estatico-clase-metodos.py
+++ basic_concepts/10-estatico-clase-metodos.py
@@ -1,18 +1,37 @@
-class MiClase:
-    atributo_de_clase = 7
+"""
+@staticmethod and @classmethod are decorators 
+used to define methods that belong to a class 
+rather than an instance of that class.
+"""
+"""
+A @staticmethod is a method that does not 
+depend on the state of the object and can be
+called without creating an instance of the class. 
+It is defined using the @staticmethod decorator 
+and takes no self parameter.
+"""
+"""
+A @classmethod is a method that operates on the class
+itself rather than on instances of the class. It is
+defined using the @classmethod decorator and takes the
+class itself as its first parameter, usually named cls.
+"""
+
+class MyClass:
+    class_attribute = 7
 
     def __init__(self, color):
         self.color = color
     
     @staticmethod
-    def metodo_estatico(color):
-        print(f"Este es un método estático del color {color}")
+    def static_method(color):
+        print(f"This is a static method of color {color}")
     
     @classmethod
-    def metodo_de_clase(cls):
+    def class_method(cls):
         print(cls.__name__)
-        # print("Este es un método de clase con el atributo de clase", cls.atributo_de_clase)
+        # print("This is a class method with the class attribute", cls.class_attribute)
 
-miclase = MiClase("Yellow")
-MiClase.metodo_estatico("Yellow")
-MiClase.metodo_de_clase()
+my_class = MyClass("Yellow")
+MyClass.static_method("Yellow")
+MyClass.class_method()
